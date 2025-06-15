1906363913b536b47429d8bccceeaaf5ed6d7717
row-huh <rohaabropathan@gmail.com>
2025-06-15 13:43:30 +0500

    nfa and dfa classes + simulation done

---
+++ dfa.py
@@ -0,0 +1,16 @@
+class DFA:
+    def __init__(self, states, alphabet, transitions, start_state, accept_states):
+        self.states = states
+        self.alphabet = alphabet
+        self.transitions = transitions
+        self.start_state = start_state
+        self.accept_states = accept_states
+
+    def simulate(self, input_string):
+        current_state = self.start_state
+        for symbol in input_string:
+            if (current_state, symbol) in self.transitions:
+                current_state = self.transitions[(current_state, symbol)]
+            else:
+                return False
+        return current_state in self.accept_states
